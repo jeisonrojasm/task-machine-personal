c8679764be7ae73ffeca701c57839ee67f446cea
Jeison Rojas <j_esteban96@hotmail.com>
2023-03-21 13:18:11 -0500

    useLocalStorage fixed

--- src/GeneralHooks/useLocalStorage.js
+++ src/GeneralHooks/useLocalStorage.js
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
 
-    const [data, setData] = useState(JSON.parse(localStorage.getItem(key)));
+    const [data, setData] = useState(initialValue);
+
+    let parsedData = localStorage.getItem(key);
 
     useEffect(() => {
-        if (!data) {
-            localStorage.setItem(key, JSON.stringify(initialValue));
-            setData(JSON.parse(localStorage.getItem(key)));
+        if (!parsedData) {
+            localStorage.setItem(key, JSON.stringify(data));
         } else {
             setData(JSON.parse(localStorage.getItem(key)));
         }
